b0809d3564dfefc1448630ef24526ba0716f4c8d
Iñaki Reta <jirsis@gmail.com>
2024-06-03 21:39:22 +0200

    junio 2024

--- ausolan/getMonth.sh
+++ ausolan/getMonth.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 #9337
 
-HOME_DIR=mayo2024
+
+echo -n "Dime el mesAño: " 
+read HOME_DIR
+
+mkdir $HOME_DIR
+
+echo "Turno: Menú Doble"
+open https://menuak.ausolan.com/
+
+echo "9337" | pbcopy
+echo -n "Copia el menú mensual de la opción Basal y pulsa 'intro' "
+read 
+pbpaste > $HOME_DIR/$HOME_DIR.json
 FILE=$HOME_DIR/$HOME_DIR.json
-ID_MENU="24195495"
-JSESSIONID="ih5vd1DIQy+1ftriInW886O5.undefined"
+
+cd $HOME_DIR
+mkdir pan postres primeros segundos
+cd ..
+
+echo -n "Copia el ID_MENU: "
+read ID_MENU
+
+echo -n "Ahora copia la cookie JSESSIONID: "
+read JSESSIONID
 
 for plato in $(cat $FILE | jq ".listadoPlatos[].primerosPlatos[].idPlatoVersion" - | sort | uniq); do
 	curl 'https://menuak.ausolan.com/menu/cargarIngredientes' -X POST -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0' -H 'Accept: */*' -H 'Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'X-Requested-With: XMLHttpRequest' -H 'Origin: https://menuak.ausolan.com' -H 'Connection: keep-alive' -H 'Referer: https://menuak.ausolan.com/menu/menuCentro' -H "Cookie: JSESSIONID=$JSESSIONID;"  --data-raw "idMenu=$ID_MENU&idPlatoVersion=$plato" > $HOME_DIR/primeros/$plato.json
@@ -26,3 +46,5 @@ for plato in $(cat $FILE | jq ".listadoPlatos[].pan.idPlatoVersion" - | sort | u
 	curl 'https://menuak.ausolan.com/menu/cargarIngredientes' -X POST -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0' -H 'Accept: */*' -H 'Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'X-Requested-With: XMLHttpRequest' -H 'Origin: https://menuak.ausolan.com' -H 'Connection: keep-alive' -H 'Referer: https://menuak.ausolan.com/menu/menuCentro' -H "Cookie: JSESSIONID=$JSESSIONID;"  --data-raw "idMenu=$ID_MENU&idPlatoVersion=$plato" > $HOME_DIR/pan/$plato.json
 	sleep 1
 done
+
+
